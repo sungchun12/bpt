a7706a274c96de519138a8ac4888ed0360dc9737
Sung Won Chung <sung@datafold.com>
2024-02-16 20:34:05 -0800

    first draft

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,51 @@
-fn main() {
-    println!("Hello, world!");
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::Write;
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Column {
+    name: String,
+    #[serde(default)]
+    tags: Vec<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Model {
+    name: String,
+    columns: HashMap<String, Column>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Manifest {
+    nodes: HashMap<String, Model>,
+}
+
+fn main() -> std::io::Result<()> {
+    let mut file = File::open("test_artifacts/manifest.json")?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+
+    let manifest: Manifest = serde_json::from_str(&contents).unwrap();
+
+    let mut out_file = File::create("schema.yml")?;
+    write!(out_file, "version: 2\n\nmodels:\n").unwrap();
+
+    for (_key, model) in manifest.nodes.iter() {
+        write!(out_file, "  - name: {}\n    columns:\n", model.name).unwrap();
+        for (name, column) in model.columns.iter() {
+            write!(
+                out_file,
+                "      - name: {}\n        tests:\n           - not_null\n           - unique\n",
+                name
+            )
+            .unwrap();
+            if !column.tags.is_empty() {
+                write!(out_file, "        tags: {:?}", column.tags).unwrap();
+            }
+        }
+    }
+
+    Ok(())
 }
